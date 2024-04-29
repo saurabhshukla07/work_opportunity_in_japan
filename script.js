5d8e3118804cd1bd953e323154bce41a6f32d4cf
Saurabh <sidshukla8416@gmail.com>
2024-04-30 00:32:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings:["a Web Developer?", "a Data Analyst?", "or Data Science Engineer 📢"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
+
